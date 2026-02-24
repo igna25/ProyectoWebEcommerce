fda1b9b2c1b5e9db5844435da77051339ae3c954
rios-matias <rios.matias998@gmail.com>
2026-02-24 17:46:05 -0300

    fix

---
+++ src/lib/Repositories/UsersRepository.ts
@@ -0,0 +1,107 @@
+import { pooler as sql } from "@/lib/db/db";
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
+import { User } from "../Entities/User";
+
+class UsersRepository {
+  async getUserById(userId: string): Promise<User | undefined> {
+    try {
+      const query = await sql<User>`SELECT * FROM users WHERE id = ${userId}`;
+      return query.rows[0];
+    } catch (error) {
+      console.error(`Failed to fetch user with ID ${userId}:`, error);
+      throw new Error(`Failed to fetch user with ID ${userId}.`);
+    }
+  }
+
+  async registerUser(
+    email: string,
+    username: string,
+    password: string,
+  ): Promise<boolean> {
+    try {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      await sql`
+        INSERT INTO users (email, username, password)
+        VALUES (${email}, ${username}, ${hashedPassword})
+      `;
+      return true;
+    } catch (error) {
+      console.error("Error al registrar el usuario:", error);
+      throw new Error("Error al registrar el usuario.");
+    }
+  }
+
+  async createUser(
+    name: string,
+    role: string,
+    email: string,
+    password: string,
+  ): Promise<string> {
+    try {
+      const userId = uuidv4();
+      await sql`
+          INSERT INTO users(id, name, role, email, password) 
+          VALUES (${userId}, ${name}, ${role}, ${email}, ${password})
+        `;
+      return userId;
+    } catch (error) {
+      console.error("Failed to create user:", error);
+      throw new Error("Failed to create user.");
+    }
+  }
+
+  async updateUser(
+    userId: string,
+    name: string,
+    role: string,
+    email: string,
+    password: string,
+  ): Promise<void> {
+    try {
+      await sql`
+          UPDATE users 
+          SET name = ${name}, role = ${role}, email = ${email}, password = ${password} 
+          WHERE id = ${userId}
+        `;
+    } catch (error) {
+      console.error("Failed to update user:", error);
+      throw new Error("Failed to update user.");
+    }
+  }
+
+  async deleteUser(userId: string): Promise<void> {
+    try {
+      await sql`
+          DELETE FROM users 
+          WHERE id = ${userId}
+        `;
+    } catch (error) {
+      console.error("Failed to delete user:", error);
+      throw new Error("Failed to delete user.");
+    }
+  }
+
+  async getAllUsers(): Promise<User[]> {
+    try {
+      const query = await sql<User>`SELECT * FROM users`;
+      return query.rows;
+    } catch (error) {
+      console.error("Failed to fetch users:", error);
+      throw new Error("Failed to fetch users.");
+    }
+  }
+
+  async getUsersByName(userName: string): Promise<User> {
+    try {
+      const query =
+        await sql<User>`SELECT * FROM users WHERE name = ${userName}`;
+      return query.rows[0];
+    } catch (error) {
+      console.error("Failed to fetch users:", error);
+      throw new Error("Failed to fetch users.");
+    }
+  }
+}
+
+export default UsersRepository;
