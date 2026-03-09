7d4229e580d7601749a14a5fcea2e43a4821381d
Ignacio Martín <ignaciomartin159@gmail.com>
2026-03-09 16:12:08 -0300

    test: implement sales API tests with Playwright

--- src/app/api/sales/[id]/route.ts
+++ src/app/api/sales/[id]/route.ts
@@ -1,13 +1,19 @@
 import SalesRepository from "@/lib/Repositories/SalesRepository";
 import { NextResponse } from "next/server";
+import { unstable_noStore } from "next/cache";
 
 export const dynamic = "force-dynamic";
 
 export async function GET(_req: any, context: { params: { id: string } }) {
+  unstable_noStore();
   try {
     const salesRepository = new SalesRepository();
     const sale = await salesRepository.getSaleById(context.params.id);
-    return NextResponse.json({ sale }, { status: 200 });
+    if (!sale) {
+      return NextResponse.json({ msg: "Sale not found" }, { status: 404 });
+    } else {
+      return NextResponse.json({ sale }, { status: 200 });
+    }
   } catch (err) {
     return NextResponse.json(
       { msg: "Error trying to fetch sale" },
