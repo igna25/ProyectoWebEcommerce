38a77da646f81da355c8d496a308272371fc4781
rios-matias <rios.matias998@gmail.com>
2026-03-09 17:00:01 -0300

    fix: build fix

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -4,7 +4,6 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import SideNav from "../ui/admin/sideBar/SideBar";
 import HeaderMobile from "../ui/admin/sideBar/HeaderMobile";
-import PrefetchAdminPage from "./PrefetchAdminPage";
 
 export default function RootLayout({
   children,
@@ -14,12 +13,11 @@ export default function RootLayout({
   return (
     <Fragment>
       <ToastContainer position="top-right" />
-      <PrefetchAdminPage />
       <SideNav></SideNav>
       <main className="flex-1">
         <div className="flex flex-col md:ml-60 sm:border-r sm:border-zinc-700 min-h-screen">
           <HeaderMobile />
-          <div className="flex flex-col pt-2 px-4 space-y-2 bg-zinc-100 flex-grow pb-4">
+          <div className="flex flex-col pt-14 md:pt-2 px-4 space-y-2 bg-zinc-100 flex-grow pb-4">
             {children}
           </div>
         </div>
